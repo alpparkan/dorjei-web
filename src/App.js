fde3e3bc3bc9a0e08edf6f3f4c177d7fde7c530c
Alp Parkan <alpparkan@gmail.com>
2021-03-18 00:41:16 +0300

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React from 'react'
+import Draggable from 'react-draggable'
+import DraggableImage from './DraggableImage'
 import './App.css';
+import logo from './logo.png';
+import polaroid1 from './polaroids/1.jpg';
+import polaroid2 from './polaroids/2.jpg';
+import polaroid3 from './polaroids/3.jpg';
+import polaroid4 from './polaroids/4.jpg';
+import polaroid5 from './polaroids/5.jpg';
+import polaroid6 from './polaroids/6.jpg';
 
 function App() {
+  const nodeRef = React.useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-logo">
+        <img className="logo" src={logo}/>
+      </div>
+
+        <div className="container-polaroid">
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid1} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid2} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid3} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid4} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid5} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+          <div>
+            <Draggable nodeRef={nodeRef}>
+              <div className="polaroid-20p">
+                <img ref={nodeRef} src={polaroid6} draggable="false"/>
+              </div>
+            </Draggable>
+          </div>
+        </div>
+
+        <div className="black-dot" style={{marginRight: 40}}></div>
+        <div className="black-dot"></div>
+        
     </div>
   );
 }
---
+++ src/DraggableImage.js
@@ -0,0 +1,19 @@
+/**
+ * Your Component
+ */
+function DraggableImage() {
+  return (
+    <div
+      style={{
+        //opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}
+    >
+      DORJEI
+    </div>
+  )
+}
+
+export default DraggableImage
