07a95e07e78472eb3818d3c5beb653aa9a0a504d
Alp Parkan <alpparkan@gmail.com>
2021-03-23 01:14:49 +0300

    add detailed modals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Draggable from 'react-draggable'
+import React from 'react';
+import Draggable from 'react-draggable';
+import Modal from 'react-modal';
 import './App.css';
 
 import logo from './logo.png';
@@ -22,8 +23,40 @@ import left1 from './polaroids/left1.jpg';
 import left2 from './polaroids/left2.jpg';
 import left3 from './polaroids/left3.jpg';
 
+import modal1 from './modal/modal1.jpeg'
+import modal2 from './modal/modal2.jpeg'
+import modal3 from './modal/modal3.jpeg'
+import modal4 from './modal/modal4.jpeg'
+
+const customStyles = {
+  overlay : {
+    zIndex: 99,
+    position: 'absolute'
+  },
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    transform             : 'translate(-50%, -50%)',
+    border                : 'none'
+  }
+};
+
 function App() {
   const nodeRef = React.useRef(null);
+  const [isModalOpen, setIsOpen] = React.useState(false);
+  const [modalSource, setModalSource] = React.useState();
+
+  function openModal(source) {
+    setModalSource(source);
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalSource();
+    setIsOpen(false);
+  }
 
   return (
     <div /* className="App" */>
@@ -43,7 +76,7 @@ function App() {
         <div className="container-polaroid">
           <Draggable nodeRef={nodeRef}>
             <div className="polaroid-20p">
-              <img ref={nodeRef} src={polaroid2} draggable="false"/>
+              <img ref={nodeRef} src={polaroid2} draggable="false" onClick={() => openModal(modal4)}/>
             </div>
           </Draggable>
         </div>
@@ -79,7 +112,7 @@ function App() {
             </Draggable>
           </div>
           <div className="two-dots">
-            <div className="black-dot"></div>
+            <div className="black-dot" style={{marginRight: '5px'}}></div>
             <div className="black-dot"></div>
           </div>
         </div>
@@ -111,7 +144,7 @@ function App() {
             <div className="container-polaroid-550p">
               <Draggable nodeRef={nodeRef}>
                 <div className="polaroid-50p">
-                  <img ref={nodeRef} src={left2} draggable="false"/>
+                  <img ref={nodeRef} src={left2} draggable="false" onClick={() => openModal(modal1)}/>
                 </div>
               </Draggable>
             </div>
@@ -122,34 +155,36 @@ function App() {
         
         <div className="container-polaroid-2-right">
           <div>
-            <div className="container-polaroid">
+            <div className="container-polaroid" style={{textAlign: 'center'}}>
               <Draggable nodeRef={nodeRef}>
                 <div className="polaroid-20p">
                   <img ref={nodeRef} src={right1} draggable="false"/>
                 </div>
               </Draggable>
             </div>
-            <div style={{marginTop: 300, textAlign: 'initial'}}>
-              <div>
+            <div style={{marginTop: 300, display: 'flex', paddingBottom: '30px'}}>
+              <div style={{display: 'flex', alignSelf: 'flex-end'}}>
+                <div className="black-dot-2"></div>
+              </div>
+              <div style={{marginLeft: '10px'}}>
                 <Draggable nodeRef={nodeRef}>
                   <div className="polaroid-85p">
-                    <img ref={nodeRef} src={right5} draggable="false"/>
+                    <img ref={nodeRef} src={right5} draggable="false" onClick={() => openModal(modal3)}/>
                   </div>
                 </Draggable>
               </div>
-              <div className="black-dot-2"></div>
             </div>
             
           </div>
           
-          <div className="container-polaroid">
+          <div className="container-polaroid" style={{textAlign: 'center'}}>
             <Draggable nodeRef={nodeRef}>
               <div className="polaroid-20p">
-                <img ref={nodeRef} src={right2} draggable="false"/>
+                <img ref={nodeRef} src={right2} draggable="false" onClick={() => openModal(modal2)}/>
               </div>
             </Draggable>
           </div>
-          <div>
+          <div style={{textAlign: 'center'}}>
             <div className="container-polaroid">
               <Draggable nodeRef={nodeRef}>
                 <div className="polaroid-20p">
@@ -165,11 +200,19 @@ function App() {
               </Draggable>
             </div>
           </div>
-          
-
 
         </div>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div className="polaroid-modal">
+          <img src={modalSource}/>
+        </div>
+      </Modal>
         
     </div>
   );
